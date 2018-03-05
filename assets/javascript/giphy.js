7d8bf60511b1787cc6b86eef33b869d0c351a23f
Alvin Balmeo <alvinbalmeo@Alvins-MacBook-Pro.local>
2018-03-04 23:52:29 -0800

    Adding updates

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,25 @@
+var array = ["DSD"]; 
+
+console.log(array); 
+
+
+
+//user input is documented in a submit form
+$("addinput").on("click", function(event){
+    event.preventDefault();
+    var input = $("#userinput").val().trim(); 
+    array.push(input); 
+    console.log(array); 
+
+});
+ 
+//user input that was documented is placed on top of page in buttons
+
+//add a general style class and apply to all buttons that are registered
+
+
+//API call 
+
+//on click function to start GIF, on click again will turn it off 
+
+
